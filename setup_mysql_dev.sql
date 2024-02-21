6bb7eedeca0caf5634425fe53e52b3a151abf674
Naasor <amofasandra7@gmail.com>
2024-02-21 22:12:21 +0000

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,13 +7,17 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
     IF NOT EXISTS 'hbnb_dev'@'localhost'
+SET PASSWORD
+    FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT USAGE
+    ON *.*
+    TO 'hbnb_dev'@'localhost'
+GRANT ALL PRIVILEDES
+    ON 'hbnb_dev_db'.*
+    TO 'hbnb_dev'@'localhost'
     IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES
-   ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT
-   ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+    ON `performance_schema`.*
+    TO 'hbnb_dev'@'localhost'
+    IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
