9b5e178cfbcd3529c47aceb3f124c68f5f53bc94
Naasor <amofasandra7@gmail.com>
2024-02-21 22:53:21 +0000

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -10,3 +10,4 @@ SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
