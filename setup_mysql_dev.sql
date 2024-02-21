83958ec72c458210e17b9b6ca187b212988148bd
Naasor <amofasandra7@gmail.com>
2024-02-21 22:39:47 +0000

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,23 +1,23 @@
 -- Creates a MySQL server with:
---   Database hbnb_test_db.
---   User hbnb_test with password hbnb_test_pwd in localhost.
---   Grants all privileges for hbnb_test on hbnb_test_db.
---   Grants SELECT privilege for hbnb_test on performance_schema.
+--   Database hbnb_dev_db.
+--   User hbnb_test with password hbnb_dev_pwd in localhost.
+--   Grants all privileges for hbnb_dev on hbnb_dev_db.
+--   Grants SELECT privilege for hbnb_dev on performance_schema.
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER
-    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD
     FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT USAGE
     ON *.*
     TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES
-   ON `hbnb_test_db`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+   ON `hbnb_dev_db`.*
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_test'@'localhost'
-   IDENTIFIED BY 'hbnb_test_pwd';
+   TO 'hbnb_dev'@'localhost'
+   IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
