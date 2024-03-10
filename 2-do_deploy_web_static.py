d8cecedb9fe71fe91c50bf061993d57f80c4d5ba
Osbert  D.  Amedzorme <132359218+oswealth@users.noreply.github.com>
2024-03-10 23:23:00 +0000

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
+"""Compress web static package"""
 from fabric.api import *
+from datetime import datetime
 from os import path
-
 
 env.hosts = ['54.236.27.34', '54.167.176.115']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
-
 def do_deploy(archive_path):
-    """Deploy web files to server
-    """
-    if not path.exists(archive_path):
-        print(f"Error: Archive '{archive_path}' not found.")
-        return False
-
+    """Deploy web files to server"""
     try:
-        # Upload the archive to the temporary folder on the server
+        if not path.exists(archive_path):
+            return False
+
         put(archive_path, '/tmp/')
 
-        # Extract the archive
-        timestamp = archive_path.split('_')[-1][:-4]
-        remote_path = '/data/web_static/releases/web_static_{}/'.format(timestamp)
+        # create target dir
+        timestamp = archive_path[-18:-4]
+        run('sudo mkdir -p /data/web_static/' \
+            'releases/web_static_{}/'.format(timestamp))
 
-        run('sudo mkdir -p {}'.format(remote_path))
-        run('sudo tar -xzf /tmp/{} -C {}'.format(path.basename(archive_path), remote_path))
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C ' \
+            '/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
 
-        # Remove the temporary archive
-        run('sudo rm /tmp/{}'.format(path.basename(archive_path)))
+        # remove archive
+        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
-        # Move contents into the web_static release folder
-        run('sudo mv {}web_static/* {}'.format(remote_path, remote_path))
+        # move contents into host web_static
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* ' \
+            '/data/web_static/releases/web_static_{}/'
+            .format(timestamp, timestamp))
 
-        # Remove the extraneous web_static directory
-        run('sudo rm -rf {}web_static'.format(remote_path))
+        # remove extraneous web_static dir
+        run('sudo rm -rf /data/web_static/releases/' \
+            'web_static_{}/web_static'
+            .format(timestamp))
 
-        # Update the symbolic link
         run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(remote_path))
 
-        print("New version deployed successfully!")
-        return True
-
-    except Exception as e:
-        print(f"Error during deployment: {e}")
+        run('sudo ln -s /data/web_static/releases/' \
+            'web_static_{}/ /data/web_static/current'.format(timestamp))
+    except:
         return False
+
+    return True
