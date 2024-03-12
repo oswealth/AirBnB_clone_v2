eb66b4f2d336e13d30d9312acd66d82230bb855c
oswealth <osbertjaywealth@gmail.com>
2024-03-12 11:36:19 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ env.hosts = ['54.236.27.34', '54.167.176.115']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
+
 def do_deploy(archive_path):
     """Deploy web files to server"""
     try:
@@ -18,10 +19,10 @@ def do_deploy(archive_path):
 
         # create target dir
         timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/' \
-            'releases/web_static_{}/'.format(timestamp))
+        run('sudo mkdir -p /data/web_static/releases/web_static_{}/'
+            .format(timestamp))
 
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C ' \
+        run('sudo tar -xzf /tmp/web_static_{}.tgz -C '
             '/data/web_static/releases/web_static_{}/'
             .format(timestamp, timestamp))
 
@@ -29,20 +30,20 @@ def do_deploy(archive_path):
         run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
         # move contents into host web_static
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* ' \
+        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* '
             '/data/web_static/releases/web_static_{}/'
             .format(timestamp, timestamp))
 
         # remove extraneous web_static dir
-        run('sudo rm -rf /data/web_static/releases/' \
-            'web_static_{}/web_static'
+        run('sudo rm -rf /data/web_static/releases/web_static_{}/web_static'
             .format(timestamp))
 
         run('sudo rm -rf /data/web_static/current')
 
-        run('sudo ln -s /data/web_static/releases/' \
-            'web_static_{}/ /data/web_static/current'.format(timestamp))
-    except:
+        run('sudo ln -s /data/web_static/releases/web_static_{}/ '
+            '/data/web_static/current'.format(timestamp))
+    except Exception as e:
+        print(f"Error during deployment: {e}")
         return False
 
     return True
